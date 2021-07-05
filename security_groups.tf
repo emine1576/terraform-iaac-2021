74c0c6a2734b05ef3b8bde488da7e025a8d05273
emine1576 <emine3310@gmail.com>
2021-07-05 02:44:46 +0000

    commit

---
+++ security_groups.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "sec" {
+  name        = "sec"
+  description = "Allow sec inbound traffic"
+  
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
