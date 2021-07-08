b5b0c73b9d9c6f0bb6f7c4648658912f2f4bf21e
emine1576 <emine3310@gmail.com>
2021-07-08 20:40:00 +0000

    changes

--- datasource.tf
+++ datasource.tf
@@ -1,8 +1,5 @@
-data "aws_availability_zones" "all" {}
 
-output "AZ" {
-  value = data.aws_availability_zones.all.names[0]
-}
+
 
 
 
--- ec2_one.tf
+++ ec2_one.tf
@@ -15,10 +15,7 @@ data "aws_ami" "ubuntu" {
 }
 
 
-variable "availability_zone_names" {
-  type    = list(string)
-  default = ["us-west-1a"]
-}
+
 
 
    
